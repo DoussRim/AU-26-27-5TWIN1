07403d95c2cb6062c82dcca7dd2161030359b4e3
DoussRim <rim.douss@esprit.tn>
2026-09-23 14:50:14 +0100

    S3

--- Event/admin.py
+++ Event/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin,messages
-from Event.models import Event
+from Event.models import *
 from datetime import datetime
 # Register your models here.
 class FilterDate(admin.SimpleListFilter):
@@ -18,16 +18,25 @@ class FilterDate(admin.SimpleListFilter):
             return queryset.filter(evt_date__gt=datetime.now())
         if self.value()=='TE':
             return queryset.filter(evt_date__exact=datetime.now())
-    
+class ParticipantAdmin(admin.TabularInline):
+    model=Participants 
+    readonly_fields=('participation_date',)
 @admin.register(Event)#decorator
 class EventAdmin(admin.ModelAdmin):
     
     list_display=('title','category','description','evt_date','creation_date',
-                  'update_date','state')
+                  'update_date','state','organizer','participant_list')
+    def participant_list(self,obj):
+        names=[p.username for p in obj.participant.all()]
+        shown=','.join(names[:2])
+        if not names:
+            return "No Participant!"
+        return shown + (f"(+{len(names)-2} more)" if len(names)>2 else "")
     search_fields=('title','evt_date')
     list_filter=['title','organizer',FilterDate]
     ordering=['-evt_date']
     list_per_page=2
+    inlines=[ParticipantAdmin]
     def accept_state(self,request,queryset):
             req=queryset.update(state=True)
             if req==1:
@@ -36,6 +45,7 @@ class EventAdmin(admin.ModelAdmin):
                 msg=f"{req} events were"
             messages.success(request,f'{msg} successfully updated')
     accept_state.short_description="State True"
+    #@admin.display(description="State False")
     def refuse_state(self,request,queryset):
         req=queryset.update(state=False)
         if req==1:
@@ -47,4 +57,20 @@ class EventAdmin(admin.ModelAdmin):
     actions=[accept_state,refuse_state]
     autocomplete_fields=['organizer']
     readonly_fields=['creation_date','update_date']
+    fieldsets=(
+        ('A propos',{
+            'fields':('description','category','image','nbe_participant')
+        }),
+        ('Event Dates',{
+                    'fields':('evt_date','creation_date','update_date')
+                }),
+        ('State of The Event',{
+                            'fields':('state',)
+                        }),
+        (None,{
+                            'fields':('organizer',)
+                        }),
+    )
+
+   
 #admin.site.register(Event,EventAdmin)
--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL="Person.Person"
+LOGIN_REDIRECT_URL="aff"
